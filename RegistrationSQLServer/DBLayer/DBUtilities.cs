5583faa66068eafd05c914663bd6db6c7bbb7570
Johnny <Johnny@DESKTOP-31T0HM6>
2020-05-19 00:26:51 -0400

    first commit

---
+++ RegistrationSQLServer/DBLayer/DBUtilities.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace RegistrationSQLServer.DBLayer
+{
+    public class DBUtilities
+    {
+        //private static SqlConnection GetSqlConnection()
+        //{
+        //    string connectionString = ConfigurationManager.ConnectionStrings["RegistrationConnectionString"].ToString();
+
+        //    return new SqlConnection(connectionString);
+
+        //}
+
+        //public static int InsertUserInformation(BusinessLayer.UserInformation ui)
+        //{
+
+        //    int result;
+
+        //    using (SqlConnection cnn = GetSqlConnection())
+        //    {
+        //        String sql = $"Insert into UserInformation(FirstName,LastName,Address,City,Province,PostalCode,Country) values ('{ui.FirstName}','{ui.LastName}','{ui.Address}','{ui.City}','{ui.Province}','{ui.PostalCode}','{ui.Country}')";
+
+        //        using (SqlCommand command = new SqlCommand(sql, cnn))
+        //        {
+        //            using (SqlDataAdapter adapter = new SqlDataAdapter())
+        //            {
+        //                cnn.Open();
+
+        //                adapter.InsertCommand = new SqlCommand(sql, cnn);
+
+        //                result = adapter.InsertCommand.ExecuteNonQuery();
+        //            }
+        //        }
+        //    }
+
+        //    return result;
+
+        //}
+
+        private static SqlConnection GetSqlConnection()
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["RegistrationConnectionString"].ToString();
+
+            return new SqlConnection(connectionString);
+
+        }
+
+        public static int InsertUserInformation(BusinessLayer.UserInformation ui)
+        {
+
+            int result;
+
+            using (SqlConnection cnn = GetSqlConnection())
+            {
+                String sql = $"INSERT INTO UserInformation(FirstName,LastName,Address,City,Province,PostalCode,Country) OUTPUT INSERTED.ID VALUES ('{ui.FirstName}','{ui.LastName}','{ui.Address}','{ui.City}','{ui.Province}','{ui.PostalCode}','{ui.Country}')";
+
+                using (SqlCommand command = new SqlCommand(sql, cnn))
+                {
+                    cnn.Open();
+
+                    command.CommandType = System.Data.CommandType.Text;
+                    command.CommandText = sql;
+                    result = (int)command.ExecuteScalar();
+                    //result = command.ExecuteNonQuery();
+                }
+            }
+
+            return result;
+
+        }
+
+        public static BusinessLayer.UserInformation SelectUserInformationById(int userId)
+        {
+            BusinessLayer.UserInformation userInformation = new BusinessLayer.UserInformation();
+
+            using (SqlConnection cnn = GetSqlConnection())
+            {
+                String sql = $"Select * From UserInformation Where Id = {userId}";
+
+                using (SqlCommand command = new SqlCommand(sql, cnn))
+                {
+                    cnn.Open();
+
+                    using (SqlDataReader dataReader = command.ExecuteReader())
+                    {
+                        while (dataReader.Read())
+                        {
+                            userInformation.Id = (int)dataReader.GetValue(0);
+                            userInformation.FirstName = (string)dataReader.GetValue(1);
+                            userInformation.LastName = (string)dataReader.GetValue(2);
+                            userInformation.Address = (string)dataReader.GetValue(3);
+                            userInformation.City = (string)dataReader.GetValue(4);
+                            userInformation.Province = (string)dataReader.GetValue(5);
+                            userInformation.PostalCode = (string)dataReader.GetValue(6);
+                            userInformation.Country = (string)dataReader.GetValue(7);
+                        }
+                    }
+                }
+            }
+
+            return userInformation;
+        }
+
+        public static int UpdateUserInformationById(int userId, BusinessLayer.UserInformation ui)
+        {
+            int result;
+
+            using (SqlConnection cnn = GetSqlConnection())
+            {
+                String sql = $"UPDATE UserInformation SET FirstName = '{ui.FirstName}', LastName = '{ui.LastName}', Address = '{ui.Address}', City = '{ui.City}', Province = '{ui.Province}', PostalCode = '{ui.PostalCode}', Country = '{ui.Country}' WHERE Id = {userId}";
+
+                using (SqlCommand command = new SqlCommand(sql, cnn))
+                {
+                    cnn.Open();
+
+                    command.CommandType = System.Data.CommandType.Text;
+                    command.CommandText = sql;
+                    result = command.ExecuteNonQuery();
+                }
+            }
+
+            return result;
+        }
+
+    }
+}
---
+++ RegistrationSQLServer/EditUserInformationById.aspx.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace RegistrationSQLServer
+{
+    public partial class EditUserInformationById : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+            {
+                int selectedID = int.Parse(Request.QueryString["id"]);
+                BusinessLayer.UserInformation selectedUserInformation = DBLayer.DBUtilities.SelectUserInformationById(selectedID);
+                firstNameTextBox.Text = selectedUserInformation.FirstName;
+                lastNameTextBox.Text = selectedUserInformation.LastName;
+                addressTextBox.Text = selectedUserInformation.Address;
+                cityTextBox.Text = selectedUserInformation.City;
+                stateOrProvinceTextBox.Text = selectedUserInformation.Province;
+                zipCodeTextBox.Text = selectedUserInformation.PostalCode;
+                countryTextBox.Text = selectedUserInformation.Country;
+            }
+        }
+
+        protected void updateInfoButton_Click(object sender, EventArgs e)
+        {
+            if (Page.IsValid)
+            {
+                int selectedID = int.Parse(Request.QueryString["id"]);
+                BusinessLayer.UserInformation newUserInfo = new BusinessLayer.UserInformation();     
+                newUserInfo.FirstName = Server.HtmlEncode(firstNameTextBox.Text);
+                newUserInfo.LastName = Server.HtmlEncode(lastNameTextBox.Text);
+                newUserInfo.Address = Server.HtmlEncode(addressTextBox.Text);
+                newUserInfo.City = Server.HtmlEncode(cityTextBox.Text);
+                newUserInfo.Province = Server.HtmlEncode(stateOrProvinceTextBox.Text);
+                newUserInfo.PostalCode = Server.HtmlEncode(zipCodeTextBox.Text);
+                newUserInfo.Country = Server.HtmlEncode(countryTextBox.Text);
+
+                // MessageBox popup to test entered information 
+                //ClientScript.RegisterStartupScript(this.GetType(), "myalert", "alert('" + newUserInfo.FirstName + "');", true);
+
+                if (DBLayer.DBUtilities.UpdateUserInformationById(selectedID, newUserInfo) == 1)
+                {
+                    this.lblResultMessage.Text = "The User Information was successfully updated in db table";
+                    Response.Redirect("Registration.aspx");
+                }
+                else
+                    this.lblResultMessage.Text = "There was an error on inserting the user information!!!!!!";
+            }
+        }
+    }
+}
